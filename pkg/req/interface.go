29c365a5fb0af8788db7408b3d128be5023ce15d
吴江法 <1152175313@qq.com>
2021-12-20 11:50:06 +0800

    添加查询排序统一参数，查询参数接口化

--- pkg/req/interface.go
+++ pkg/req/interface.go
@@ -13,3 +13,29 @@ type Binder func(ctx *gin.Context, obj interface{}) error
 type CustomizeBinder interface {
 	Bind(ctx *gin.Context) error
 }
+
+//PageAble 用于分页查询
+type PageAble interface {
+	//PNumber 表示第几页
+	PNumber() int
+	//PSize 表示每页长度
+	PSize() int
+}
+
+//SearchAble 用于模糊查询
+type SearchAble interface {
+	//Key 表示用于模糊查询的键
+	Key() string
+	//Value 表示模糊查询的值
+	Value() interface{}
+	//ToMap 用于将SearchAble转换为一个map[string]interface{}
+	ToMap() map[string]interface{}
+}
+
+//SortAble 用于查询结果排序
+type SortAble interface {
+	//Key 表示用于排序的键
+	Key() string
+	//IsAsc 表示是否是升序排序
+	IsAsc() bool
+}
--- pkg/req/models.go
+++ pkg/req/models.go
@@ -1,25 +1,57 @@
 package req
 
-//Paging 用于分页查询
+var _ PageAble = &Paging{}
+
 type Paging struct {
 	Page     int `json:"page" form:"page" validate:"omitempty,gt=0"`
 	PageSize int `json:"pageSize" form:"pageSize" validate:"required_with=Page,gt=0"`
 }
 
-//Search 用于模糊查询
-type Search struct {
-	SearchKey   string `json:"searchKey" form:"searchKey" validate:"omitempty"`
-	SearchValue string `json:"searchValue" form:"searchValue" validate:"required_with=SearchKey"`
+func (p *Paging) PNumber() int {
+	return p.Page
 }
 
-//ToMap 用于将Search转换为一个map对象
-func (s *Search) ToMap() map[string]string {
+func (p *Paging) PSize() int {
+	return p.PageSize
+}
+
+var _ SearchAble = &Search{}
+
+type Search struct {
+	SearchKey   string      `json:"searchKey" form:"searchKey" validate:"omitempty"`
+	SearchValue interface{} `json:"searchValue" form:"searchValue" validate:"required_with=SearchKey"`
+}
+
+func (s *Search) Key() string {
+	return s.SearchKey
+}
+
+func (s *Search) Value() interface{} {
+	return s.SearchValue
+}
+
+func (s *Search) ToMap() map[string]interface{} {
 	if s == nil {
 		return nil
 	}
-	return map[string]string{
+	return map[string]interface{}{
 		s.SearchKey: s.SearchValue,
 	}
+}
+
+var _ SortAble = &Sort{}
+
+type Sort struct {
+	Asc       bool   `json:"asc" form:"asc" validate:"omitempty"`
+	SortField string `json:"sortField" form:"sortField" validate:"required_with=Asc"`
+}
+
+func (s *Sort) Key() string {
+	return s.SortField
+}
+
+func (s *Sort) IsAsc() bool {
+	return s.Asc
 }
 
 //BatchReq 用于批量操作，Infos数组中一般为某资源的id，常见的适用场景：批量删除资源
